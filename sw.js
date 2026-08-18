d93ad9ee51eaa2f31ea34c7e34e961d98b05ad49
dellangley1971-svg <dellangley1971@gmail.com>
2026-08-18 20:44:56 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+﻿const CACHE_NAME = 'mtg-tokens-v1';
+
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
